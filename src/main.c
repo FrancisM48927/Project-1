617ecee9ebd0da9373a97860c1412a412d62abc2
Francis Mailom <franzm@udel.edu>
2026-09-18 00:09:26 -0400

    Fixed debounce

--- src/main.c
+++ src/main.c
@@ -74,7 +74,7 @@ int main(void)
         }
         else if (!(GPIOC->IDR & (0x1 << USER_BTN)) && BTN_LOCK) 
         { // Debounce check if button is not active lock is on
-            Delay_Count(367676);
+            Delay_Count(267670);
             BTN_LOCK = false;
         }
     }
